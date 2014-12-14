cf4a6848c0a5eb423703c912281dca80c2bb1418
kimhyunkang <kimhyunkang@gmail.com>
2014-12-15 08:24:58 +0900

    Initial documentation

---
+++ doc/implementors/core/ops/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['yaml'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='yaml/parser/struct.InternalEvent.html' title='yaml::parser::InternalEvent'>InternalEvent</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='yaml/parser/struct.YamlBaseParser.html' title='yaml::parser::YamlBaseParser'>YamlBaseParser</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='yaml/emitter/struct.YamlBaseEmitter.html' title='yaml::emitter::YamlBaseEmitter'>YamlBaseEmitter</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='yaml/document/struct.YamlDocument.html' title='yaml::document::YamlDocument'>YamlDocument</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['yaml'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='yaml/error/struct.YamlError.html' title='yaml::error::YamlError'>YamlError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
